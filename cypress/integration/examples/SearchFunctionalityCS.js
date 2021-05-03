2063b17126638794e3e03793ed8fa4f67a13eef7
ondrejbozek <bozeko13@gmail.com>
2021-05-03 22:27:04 +0200

    CaseStudy_CS

---
+++ cypress/integration/examples/SearchFunctionalityCS.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+describe('Test search functionality', () => {
+    it('Login to the George website', () => {
+        cy.visit('https://george.csas.cz/?login_hint=7777777777') // Go to George website with hint
+        cy.title().should('eq','Přihlášení | Česká spořitelna') // Verify title of the website
+        cy.wait(1000) // Wait for page script be loaded
+        
+        /*cy.get('.ng-isolate-scope > .font-bold').click()
+        cy.wait(1000) // Wait for page script be loaded
+        cy.get('#btn\.continue > .flex').click()*/
+        cy.get('body').type('{enter}') // Click Enter to login
+        
+        cy.title().should('eq','George') // Verify title of the website
+        cy.wait(500)
+        cy.get('#readBtn').click() // Message read 
+    })
+
+    it('Search for term with one result and show detail', () => {
+        cy.get('[data-cy=overview-search-bar]').type('DM') // Search for "DM"
+        cy.wait(1000) //
+        cy.get('[data-cy=transaction-list-item-100000435868666]').click()
+        cy.get('.mr-auto').scrollIntoView().should('include.text','Únor 2016')
+        cy.wait(1000)
+        cy.get('.clearable-input--3mLh1HA6 > .g-button > .ico-error').click()
+    })
+
+    it('Show suggestion in searchbar', () => {
+        cy.get('[data-cy=search-keyword]').click() // Click on searchbar to see suggestions
+        cy.get('[data-cy=search-dropdown-item-lastWeek]').should('be.visible')
+        cy.wait(1000) // See suggestions
+    })
+
+    it('Search for term with more results', () => {
+        cy.get('[data-cy=search-keyword]').type('Liftago') // Search for "Liftago"
+        cy.get('body').type('{enter}') // Click Enter to search
+        cy.get('.g-button > .ico-error').should('be.visible')
+    })
+
+    it('Print searched data', () => {
+        cy.get('[data-cy=print-link]').click() // Click on prepare to print button
+        cy.get('[data-cy=print-button]').scrollIntoView().click() //Click on Print button
+        cy.get('.g-status-info-title').should('include.text','Data k tisku jsou připravena.')
+    })
+  })
